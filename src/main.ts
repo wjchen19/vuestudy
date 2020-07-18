74eafb4e5bdf22bb442ae794a4b9665ac4dcf659
wjchen <997231065@qq.com>
2020-07-18 15:07:03 +0800

    初始化化

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import VueCompositionApi from "@vue/composition-api";
 Vue.config.productionTip = false;
-
+Vue.use(VueCompositionApi);
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    主页 登录页面
+    <div @click='jump'>
+      点我
+    </div>
+    <div>{{moduleName}}</div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  import {
+    ref,
+    reactive
+  } from '@vue/composition-api'
+  export default {
+    name: 'Home',
+    components: {},
+    setup() {
+      const moduleName = ref(null)
+      moduleName.value = 'dengsadljgaslfas'
+      return {
+        moduleName
+      }
+    },
+    methods: {
+      jump() {
+        console.log('你个憨批')
+      }
+    }
   }
-};
 </script>
